e2606b8f6a2f4f1ab5ee3510e0f9e5e061237998
Nat <nat@cmmc.io>
2019-05-13 12:55:55 +0700

    first update.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 const SerialPort = require("serialport");
-const Delimiter = SerialPort.parsers.Delimiter;
-const port = new SerialPort(process.env.TARGET_PORT, {
-  baudRate: parseInt(process.env.TARGET_BAUDRATE) || 115200,
+const Delimiter = require("@serialport/parser-delimiter");
+
+const port = new SerialPort(
+    process.env.PORT || "/dev/tty.SLAB_USBtoUART", {
+      baudRate: parseInt(process.env.TARGET_BAUDRATE) || 115200,
+    });
+const parser = port.pipe(new Delimiter({delimiter: "\r\n"}));
+
+let sum = 0x04 + "M" + 50 + 40;
+const msg = Buffer.from(
+    [0x02, 0x02, 0x02, "C", 0x04, "M", 50, 40, sum, 0x03]);
+
+setInterval(() => {
+  port.write(msg, err => {
+    if (err)
+      console.log(`serial.write error`, err);
+  });
+}, parseInt(process.env.EVERY_MS) || 500);
+
+port.on("open", () => {
+  console.log("port opened.");
+});
+
+parser.on("data", (data) => {
+  console.log(`Recv: `, data);
 });
